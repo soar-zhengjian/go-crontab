add076da37439630b1563dbf6fb97bcd65ba8551
Sam <zhi1125272962@qq.com>
2018-11-05 22:01:09 +0800

    add file

---
+++ prepare/etcdDel/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"time"
+	"fmt"
+)
+
+var (
+	config clientv3.Config
+	client *clientv3.Client
+	err error
+	kv clientv3.KV
+	delResp *clientv3.DeleteResponse
+	kvpair *mvccpb.KeyValue
+)
+
+func main()  {
+	config = clientv3.Config{
+		Endpoints:[]string{"127.0.0.1:2379"},
+		DialTimeout:5*time.Second,
+	}
+
+	//建立一个客户端
+	if client, err = clientv3.New(config); err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	
+
+}
