b4fb7c3794079a7e1e2863bb11af8059c36eb05a
Sam <zhi1125272962@qq.com>
2018-11-19 10:40:58 +0800

    add file

--- master/ApiServer.go
+++ master/ApiServer.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net"
 	"time"
+	"go-crontab/common"
+	"encoding/json"
 )
 
 //任务的HTTP接口
@@ -16,11 +18,92 @@ var (
 	G_apiServer *ApiServer
 )
 //保存任务接口
-func handleJobSave(w http.ResponseWriter, r *http.Request)  {
+//POST job={"name":"job1","command":"echo hello", "cronExpr":"* * * * *"}
+func handleJobSave(resp http.ResponseWriter, req *http.Request)  {
+	var (
+		err error
+		postJob string
+		job common.Job
+		oldJob *common.Job
+	)
+
+	//1,解析POST表单
+	if err = req.ParseForm(); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//2,取表单中的job字段
+	postJob = req.PostForm.Get("job")
+
+	//3,反序列化job
+	if err = json.Unmarshal([]byte(postJob),&job); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//4,保存到etcd
+	if oldJob, err = G_jobMgr.SaveJob(&job); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//正常应答
+	common.ResponseErr(resp,0,"success",oldJob)
+	return
 
 }
+
+//删除任务接口
+//POST /job/delete name=job1
+func handleJobDelete(resp http.ResponseWriter, req *http.Request)  {
+	var (
+		err error
+		name string
+		oldJob *common.Job
+	)
+	//POST: a=1&b=2&c=3
+	if err = req.ParseForm(); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//删除的任务名
+	name = req.PostForm.Get("name")
+
+	//去删除任务
+	if oldJob,err = G_jobMgr.DeletedJob(name); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//正常应答
+	common.ResponseErr(resp,0,"success",oldJob)
+	return
+}
+
+//列举所有crontab任务
+func handleJobList(resp http.ResponseWriter, req *http.Request)  {
+	var(
+		jobList []*common.Job
+		err error
+	)
+
+	//获取任务列表
+	if jobList, err = G_jobMgr.ListJobs(); err != nil{
+		common.ResponseErr(resp,-1,err.Error(),nil)
+		return
+	}
+
+	//正常应答
+	common.ResponseErr(resp,0,"success",jobList)
+	return
+}
+
+//强制杀死某个任务
+
 //初始化服务
-func InitApiServer()  {
+func InitApiServer() (err error) {
 	var (
 		mux *http.ServeMux
 		listener net.Listener
--- master/main/master.go
+++ master/main/master.go
@@ -1,14 +1,65 @@
 package main
 
-import "runtime"
+import (
+	"runtime"
+	"flag"
+	"go-crontab/master"
+	"time"
+	"fmt"
+	"go-crontab/common"
+)
 
+var (
+	confFile string //配置文件路径
+	err error
+)
+
+//解析命令行参数
+func initArgs()  {
+	// master -config ./master.json -xxx 123 -yyy ddd
+	flag.StringVar(&confFile,"config","./master.json","指定master.json")
+	flag.Parse()
+}
+//初始化线程数量
 func initEnv()  {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 func main()  {
+	//初始化命令行参数
+	initArgs()
+
 	//初始化线程
 	initEnv()
 
-	//启动Api Http服务
+	//加载配置
+	if err = master.InitConfig(confFile); err != nil{
+		common.FmtErr(err)
+	}
 
+	//初始化服务发现模块
+	if err = master.InitWorkerMgr(); err != nil{
+		common.FmtErr(err)
+	}
+
+	//日志管理器
+	if err = master.InitLogMgr(); err != nil{
+		common.FmtErr(err)
+	}
+
+	//任务管理器
+	if err = master.InitJobMgr(); err != nil{
+		common.FmtErr(err)
+	}
+
+	//启动Api Http服务
+	if err = master.InitApiServer(); err != nil{
+		common.FmtErr(err)
+	}
+
+	//正常退出
+	for{
+		time.Sleep(1*time.Second)
+	}
+	return
 }
+
